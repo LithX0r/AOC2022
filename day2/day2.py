7c60b146bafabdac1eeccc384234bde38d9c76d4
LithX0r <leon.schnetzer@gmail.com>
2022-12-02 14:17:17 +0100

    day2 finished

--- day2/day2.py
+++ day2/day2.py
@@ -1,10 +1,32 @@
 f = open('InDay2.txt', 'r')
+total = 0
+for line in f:
+    val = line.replace('\n', "").split(' ')
+    if ord(val[0]) - ord('A') == ord(val[1]) - ord('X'):
+        total += 3
+    elif (val[0] == 'A' and val[1] == 'Y') or (val[0] == 'B' and val[1] == 'Z') or (val[0] == 'C' and val[1] == 'X'):
+        total += 6
+    else:
+        total += 0
+    total += ord(val[1]) - ord('X') + 1
+f.close()
 
+print("Day2 first star: " + str(total))
+
+g = open('InDay2.txt', 'r')
 total = 0
 
-arr = f.readline().split("\n")
+for line in g:
+    val = line.replace('\n', "").split(' ')
+    res = (ord(val[1]) - ord('X'))*3
+    total += res
+    if res == 6:
+        total += (ord(val[0]) - ord('A') + 1) % 3 + 1
+    elif res == 3:
+        total += ord(val[0]) - ord('A') +1
+    else:
+        total += (ord(val[0]) - ord('A') - 1) % 3 + 1
 
-for line in arr:
-    val = line.split(" ")
-    if val[0] == val[1]:
-        total += 1
+print("Day2 second star: " + str(total))
+g.close()
+
